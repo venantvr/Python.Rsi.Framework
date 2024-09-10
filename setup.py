b8ae541871246167807778c2c69a4c16946ad75c
venantvr <r.venant.valery@gmail.com>
2024-09-10 08:39:35 +0200

    First commit...

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='Python.Rsi.venantvr',  # Nom de votre projet
+    name='venantvr',  # Nom de votre projet
     version='0.1.0',  # Version initiale
     author='venantvr',  # Votre nom ou celui de votre organisation
     author_email='r.venant.valery@gmail.com',  # Votre adresse email
@@ -9,7 +9,7 @@ setup(
     long_description=open('README.md').read(),  # Description longue, souvent le contenu de README.md
     long_description_content_type='text/markdown',  # Type de contenu pour la longue description
     url='https://github.com/venantvr/Python.Rsi.Tools.git',  # URL du projet (par exemple, GitHub)
-    packages=find_namespace_packages(include=['Python.Rsi.venantvr.*']),  # Trouve tous les packages dans le répertoire courant
+    packages=find_namespace_packages(include=['venantvr.*']),  # Trouve tous les packages dans le répertoire courant
     classifiers=[
         'Programming Language :: Python :: 3',  # Compatible avec Python 3
         'License :: OSI Approved :: MIT License',  # Type de licence
